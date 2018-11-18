000cdddb46b56453d3c25c8133bf20c2c0af17be
Deng Li <ji3g4kami@gmail.com>
2018-11-18 11:41:09 +0800

    [add] start button can trigger timer

--- BootsNSocks/Controller/GameViewController.swift
+++ BootsNSocks/Controller/GameViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class GameViewController: UIViewController {
+    
+    var trackOneViewController: TrackViewController?
 
 
     @IBOutlet weak var startButton: DesignableButton!
@@ -18,9 +20,16 @@ class GameViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toTrackOne" {
+            self.trackOneViewController = segue.destination as? TrackViewController
+        }
+    }
+    
     
     @IBAction func startButtonPressed(_ sender: DesignableButton) {
         print("hello")
+        trackOneViewController?.runTimer()
     }
     
 
